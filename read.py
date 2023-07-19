a27907420e2b18baf1131769a26b6da8b5746615
0608liang <marc.cl.kuo@gmail.com>
2023-07-19 22:00:37 +0800

    add new function

--- read.py
+++ read.py
@@ -6,8 +6,18 @@ with open('reviews.txt', 'r')as f:
 		count += 1 # 等同於count = count + 1
 		if count % 1000 == 0 :
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-print(data[0])
-print('--------------------------')
-prin(data[1])
+sum_data = 0
+for d in data :
+	len(d)
+	sum_data = sum_data + len(d)
+
+print('全部留言的平均字數為', sum_data / len(data), '個字')
+
+new = []
+for d in data :
+	if len(d) < 100 :
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100個字')
+print(new[0])
